660cff19a84b3c79dd8303860eccca7e4e2cac6a
Vasiliy13 <xxxxStelsxxxx@gmail.com>
2019-10-13 17:28:06 +0300

    completed lesson 7. Evaluation

--- parsing.c
+++ parsing.c
@@ -24,6 +24,41 @@ void add_history(char* unused) {}
 
 #endif
 
+long eval_op(long x, char* op, long y) {
+    if (!strcmp(op, "+")) {
+        return x + y;
+    } else if (!strcmp(op, "-")) {
+        return x - y;
+    } else if (!strcmp(op, "*")) {  
+        return x * y;
+    } else if (!strcmp(op, "/")) {
+        return x / y;
+    } else if (!strcmp(op, "%")) {
+        return x % y;
+    } else if (!strcmp(op, "^")) {
+        return pow(x, y);
+    }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    if (!strcmp(op, "-") && !strstr(t->children[3]->tag, "expr")) {
+        return -x;
+    }
+    
+    for (size_t i = 3; strstr(t->children[i]->tag, "expr"); i++) {
+        x = eval_op(x, op, eval(t->children[i]));
+    }
+    return x;
+}
+
 int main(int argc, char** argv) {
     mpc_parser_t* Number = mpc_new("number");
     mpc_parser_t* Operator = mpc_new("operator");
@@ -33,7 +68,7 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
         "                                                       \
             number   : /-?[0-9]+/ ;                             \
-            operator : '+' | '-' | '*' | '/' | '%';             \
+            operator : '+' | '-' | '*' | '/' | '%' | '^';       \
             expr     : <number> | '(' <operator> <expr>+ ')' ;  \
             lispy    : /^/ <operator> <expr>+ /$/ ;             \
         ",
@@ -49,7 +84,9 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r))
         {
-            mpc_ast_print(r.output);
+            // mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
