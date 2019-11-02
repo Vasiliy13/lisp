a6453ceefc2bfeb09303da0556e4707088efc955
Vasiliy13 <xxxxStelsxxxx@gmail.com>
2019-11-03 02:10:53 +0300

    Complete chapter 13. Conditionals.

--- parsing.c
+++ parsing.c
@@ -53,6 +53,8 @@ typedef struct lenv lenv;
 
 typedef lval* (*lbuiltin)(lenv* , lval* );
 
+lval* lval_eval(lenv* e, lval* v);
+
 enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_FUN, LVAL_SEXPR, LVAL_QEXPR };
 
 char* ltype_name(int t) {
@@ -248,6 +250,39 @@ lval* lval_take(lval* v, int i) {
   return x;
 }
 
+int lval_eq(lval* x, lval* y) {
+  if (x->type != y->type) {
+    return 0;
+  }
+  
+  switch (x->type) {
+    case LVAL_NUM:
+      return x->num == y->num;
+    case LVAL_ERR:
+      return strcmp(x->err, y->err) == 0;
+    case LVAL_SYM:
+      return strcmp(x->sym, y->sym) == 0;
+    case LVAL_FUN:
+      if (x->builtin || y->builtin) {
+        return x->builtin == y->builtin;
+      } else {
+        return lval_eq(x->formals, y->formals) && lval_eq(x->body, y->body);
+      }
+    case LVAL_SEXPR:
+    case LVAL_QEXPR:
+      if (x->count != y->count) {
+        return 0;
+      }
+      for (size_t i = 0; i < x->count; i++) {
+        if (!lval_eq(x->cell[i], y->cell[i])) {
+          return 0;
+        }
+      }
+      return 1;
+  }
+  return 0;
+}
+
 void lval_print(lval* v);
 
 void lval_print_expr(lval* v, char open, char close) {
@@ -376,6 +411,83 @@ void lenv_def(lenv* e, lval* k, lval* v) {
   lenv_put(e, k, v);
 }
 
+lval* builtin_ord(lenv* e, lval* a, char* op) {
+  LASSERT_NUM(op, a, 2);
+  LASSERT_TYPE(op, a, 0, LVAL_NUM);
+  LASSERT_TYPE(op, a, 1, LVAL_NUM);
+  
+  int r;
+
+  if (strcmp(op, ">") == 0) {
+    r = a->cell[0]->num > a->cell[1]->num;
+  } else if (strcmp(op, "<") == 0) {
+    r = a->cell[0]->num < a->cell[1]->num;
+  } else if (strcmp(op, ">=") == 0) {
+    r = a->cell[0]->num >= a->cell[1]->num;
+  } else if (strcmp(op, "<=") == 0) {
+    r = a->cell[0]->num <= a->cell[1]->num;
+  }
+  lval_del(a);
+  return lval_num(r);
+}
+
+lval* builtin_gt(lenv* e, lval* a) {
+  return builtin_ord(e, a, ">");
+}
+
+lval* builtin_lt(lenv* e, lval* a) {
+  return builtin_ord(e, a, "<");
+}
+
+lval* builtin_ge(lenv* e, lval* a) {
+  return builtin_ord(e, a, ">=");
+}
+
+lval* builtin_le(lenv* e, lval* a) {
+  return builtin_ord(e, a, "<=");
+}
+
+lval* builtin_cmp(lenv* e, lval* a, char* op) {
+  LASSERT_NUM(op, a, 2);
+  int r;
+  if (strcmp(op, "==") == 0) {
+    r = lval_eq(a->cell[0], a->cell[1]);
+  } else if (strcmp(op, "!=") == 0) {
+    r = !lval_eq(a->cell[0], a->cell[1]);
+  }
+  lval_del(a);
+  return lval_num(r);
+  
+}
+
+lval* builtin_eq(lenv* e, lval* a) {
+  return builtin_cmp(e, a, "==");
+}
+
+lval* builtin_ne(lenv* e, lval* a) {
+  return builtin_cmp(e, a, "!=");
+}
+
+lval* builtin_if(lenv* e, lval* a) {
+  LASSERT_NUM("if", a, 3);
+  LASSERT_TYPE("if", a, 0, LVAL_NUM);
+  LASSERT_TYPE("if", a, 1, LVAL_QEXPR);
+  LASSERT_TYPE("if", a, 2, LVAL_QEXPR);
+
+  lval* x;
+
+  a->cell[1]->type = LVAL_SEXPR;
+  a->cell[2]->type = LVAL_SEXPR;
+
+  if (a->cell[0]->num) {
+    x = lval_eval(e, lval_pop(a, 1));
+  } else {
+    x = lval_eval(e, lval_pop(a, 2));
+  }
+  lval_del(a);
+  return x;
+}
+
 lval* builtin_var(lenv* e, lval* a, char* func) {
   LASSERT_TYPE(func, a, 0, LVAL_QEXPR);
   
@@ -409,8 +521,6 @@ lval* builtin_def(lenv* e, lval* a) {
 lval* builtin_put(lenv* e, lval* a) {
   return builtin_var(e, a, "=");
 }
-
-lval* lval_eval(lenv* e, lval* v);
 
 lval* builtin_list(lenv* e, lval* a) {
   a->type = LVAL_QEXPR;
@@ -614,6 +724,14 @@ void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
 }
 
 void lenv_add_builtins(lenv* e) {
+  lenv_add_builtin(e, "if", builtin_if);
+  lenv_add_builtin(e, "==", builtin_eq);
+  lenv_add_builtin(e, "!=", builtin_ne);
+  lenv_add_builtin(e, ">",  builtin_gt);
+  lenv_add_builtin(e, "<",  builtin_lt);
+  lenv_add_builtin(e, ">=", builtin_ge);
+  lenv_add_builtin(e, "<=", builtin_le);
+
   lenv_add_builtin(e, "list", builtin_list);
   lenv_add_builtin(e, "head", builtin_head);
   lenv_add_builtin(e, "tail", builtin_tail);
